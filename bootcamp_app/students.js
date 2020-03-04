1f6212d466bee16b20e12240a5b801f290759365
Najib <najib.muqeem@gmail.com>
2020-03-04 22:21:06 +0000

    prevented SQL injection in js files

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,23 +7,20 @@ const pool = new Pool({
   database: "bootcampx"
 });
 
-const cohort = process.argv[2];
-const limit = process.argv[3];
+const cohort = `%${process.argv[2]}%`;
+const limit = process.argv[3] || 5;
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-pool
-  .query(
-    `
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${cohort}%'
-LIMIT ${limit || 5};
-`
-  )
-  .then(res => {
-    res.rows.forEach(user => {
-      console.log(
-        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
-      );
-    });
+pool.query(queryString, [cohort, limit]).then(res => {
+  res.rows.forEach(user => {
+    console.log(
+      `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+    );
   });
+});
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,22 +7,19 @@ const pool = new Pool({
   database: "bootcampx"
 });
 
-const cohort = process.argv[2];
+const cohort = process.argv[2] || "JUL02";
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`;
 
-pool
-  .query(
-    `
-    SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-    FROM teachers
-    JOIN assistance_requests ON teacher_id = teachers.id
-    JOIN students ON student_id = students.id
-    JOIN cohorts ON cohort_id = cohorts.id
-    WHERE cohorts.name = '${cohort || "JUL02"}'
-    ORDER BY teacher;
-`
-  )
-  .then(res => {
-    res.rows.forEach(row => {
-      console.log(`${row.cohort}: ${row.teacher}`);
-    });
+pool.query(queryString, [cohort]).then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   });
+});
